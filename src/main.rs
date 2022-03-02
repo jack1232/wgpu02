b91f6363359afe5cc3152925e7638429fbbb91cf
jack <jackjack5810@yahoo.com>
2022-03-01 19:08:01 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::Window,
+};
+
+fn main() {
+    let instances = wgpu::Instance::new(wgpu::Backends::all());
+    for adapter in instances.enumerate_adapters(wgpu::Backends::all()) {
+        println!("{:?}", adapter1.get_info())
+    }
+
+    let event_loop = EventLoop::new();
+    let window = Window::new(&event_loop).unwrap();
+    window.set_title("my window");
+    env_logger::init();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        }
+    });
+}
