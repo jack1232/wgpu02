61cf29943fdbd1b0483039d7d73adbd2ec0b4248
jack1232 <jackjack5810@yahoo.com>
2023-09-04 09:39:29 -0400

    update to 0.17

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ use winit::{
 };
 
 fn main() {
-    let instances = wgpu::Instance::new(wgpu::Backends::all());
+    let instances = wgpu::Instance::new(wgpu::InstanceDescriptor{
+        backends:wgpu::Backends::all(),
+        dx12_shader_compiler: Default::default(),
+    });
+
     for adapter in instances.enumerate_adapters(wgpu::Backends::all()) {
         println!("{:?}", adapter.get_info())
     }
